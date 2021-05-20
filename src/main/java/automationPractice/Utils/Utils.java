18bf23db03f4f0df362298603966967d303d34d7
vijaya <chippa.vl@gmail.com>
2021-05-20 18:17:03 +0100

    code added

--- src/main/java/automationPractice/Utils/Utils.java
+++ src/main/java/automationPractice/Utils/Utils.java
@@ -1,10 +1,11 @@
 package automationPractice.Utils;
 
 import automationPractice.Pages.BasePage;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
-public class Utils extends BasePage {
+public class Utils<Public> extends BasePage {
 
     public static void waitForPageLoad() {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -13,4 +14,8 @@ public class Utils extends BasePage {
     public static String getPageTitle() {
         return driver.getTitle();
     }
-}
+
+    //Modified code
+    public static boolean isDisplayed(WebElement element) {return element.isDisplayed();}
+ }
+
